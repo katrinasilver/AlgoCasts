fe836c7488822eb419ea6608cc7d5fc0e0912215
katrinasilver <katrina.agustin@gmail.com>
2019-03-15 21:09:37 -0700

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // let vowel = ['a', 'e', 'i', 'o', 'u']
+  // let counter = 0
+
+  // for (let char of str.toLowerCase()) {
+  //   if (vowel.includes(char)) counter++
+  // }
+
+  // return counter
+
+  let regex = new RegExp(/[aeiou]/gi)
+  const vowel = str.match(regex)
+  return vowel ? vowel.length : 0
+}
 
 module.exports = vowels;
