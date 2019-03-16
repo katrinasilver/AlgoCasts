aff9808ce970e95b25d6e724337a2c627ffbec56
katrinasilver <katrina.agustin@gmail.com>
2019-03-15 20:28:48 -0700

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let frequent = ''
+  let counter = 0
+
+  // let chars = {}
+  // for (let c of str) {
+  //   if(chars[c]) chars[c]++
+  //   else chars[c] = 1
+  // }
+  
+  let chars = str.split('').reduce((acc, val) => {
+    if (acc[val]) acc[val]++
+    else acc[val] = 1
+    return acc
+  }, {})
+
+  for(let char in chars) {
+    if (chars[char] > counter) {
+      counter = chars[char]
+      frequent = char
+    }
+  }
+
+  return frequent
+}
 
 module.exports = maxChar;
